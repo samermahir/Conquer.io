97c53f9e29e2c4e662b593c6432022b00a0749c1
Samer Mahir <samermahir@yahoo.com>
2023-03-15 16:06:43 -0700

    client side api axios

---
+++ client/src/utils/AxiosAPI.js
@@ -0,0 +1,31 @@
+// import axios from 'axios';
+
+// // Setup so when "upload file" is clicked itll render map and send user data to the backend & MySql 
+
+// const userData = {
+//     name: '',
+//     stageNumber: 0,
+//     stageName: '',
+//     address: '',
+//     zip: '',
+//     city: '',
+//     state: '',
+//     latitude: 0,
+//     longitude: 0,
+//   };
+
+// axios.get('http://localhost:3001/userData')
+//   .then(response => {
+//     console.log(response.data);
+//   })
+//   .catch(error => {
+//     console.error(error);
+//   });
+
+//   axios.post('http://localhost:3001/userData', userData)
+//   .then(response => {
+//     console.log(response.data);
+//   })
+//   .catch(error => {
+//     console.error(error);
+//   });
