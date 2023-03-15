6915026c7e393b06625d51277095ab9239206b08
Samer Mahir <samermahir@yahoo.com>
2023-03-15 16:17:16 -0700

    server routes axios

--- server/routes/api/clientRoutes.js
+++ server/routes/api/clientRoutes.js
@@ -1,35 +1,35 @@
-const router = require('express').Router();
+// const router = require('express').Router();
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: ''
-  });
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: ''
+//   });
 
-router.get('/userData', (req, res) => {
-    const sql = 'SELECT * FROM users';
+// router.get('/userData', (req, res) => {
+//     const sql = 'SELECT * FROM users';
   
-    connection.query(sql, (err, result) => {
-      if (err) {
-        res.status(500).send({ error: 'Error fetching users' });
-      } else {
-        res.status(200).send(result);
-      }
-    });
-  });
+//     connection.query(sql, (err, result) => {
+//       if (err) {
+//         res.status(500).send({ error: 'Error fetching users' });
+//       } else {
+//         res.status(200).send(result);
+//       }
+//     });
+//   });
 
-  router.post('/userData', (req, res) => {
-    const userData = req.body;
-    const sql = 'INSERT INTO users SET ?';
+//   router.post('/userData', (req, res) => {
+//     const userData = req.body;
+//     const sql = 'INSERT INTO users SET ?';
   
-    connection.query(sql, userData, (err, result) => {
-      if (err) {
-        res.status(500).send({ error: 'Error inserting user' });
-      } else {
-        res.status(200).send({ message: ' successful' });
-      }
-    });
-  });
+//     connection.query(sql, userData, (err, result) => {
+//       if (err) {
+//         res.status(500).send({ error: 'Error inserting user' });
+//       } else {
+//         res.status(200).send({ message: ' successful' });
+//       }
+//     });
+//   });
 
-  module.exports = router;
+//   module.exports = router;
